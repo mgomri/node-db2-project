f1e4576381d445b520ce430d36060fa9a81eb4de
mgomri <mel77gomri@gmail.com>
2020-05-13 01:08:37 -0400

    shema written via knex migration

---
+++ migrations/20200513002427_create-cars-table.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', tbl => {
+      tbl.increments();
+
+      tbl.string('VIN')
+      .unique()
+      .notNullable();
+      
+      tbl.string('make')
+      .notNullable();
+
+      tbl.string('model')
+      .notNullable();
+
+      tbl.integer('milage')
+      .notNullable();
+      
+      tbl.string('transmission');
+      tbl.string('title');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars');
+};
